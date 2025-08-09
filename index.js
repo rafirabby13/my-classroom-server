1a1ccb92c4d838d4c970fee182146c4d5274efde
rafi <rafiahmedrabby282@gmail.com>
2025-08-09 14:17:18 +0600

    added features

--- index.js
+++ index.js
@@ -28,10 +28,21 @@ async function run() {
         const userCollection = client.db("myClassroom").collection("users");
 
         app.get('/all-classes', async (req, res) => {
-            const classes = await classCollection.find({}).toArray()
-            // console.log(classes)
-            res.send(classes)
-        })
+            const email = req.query.email;
+            // console.log(email);
+
+            if (!email) {
+                return res.status(400).send("Email is required");
+            }
+
+            // Search in the students array
+            const classes = await classCollection.find({
+                students: email  // MongoDB automatically searches in arrays
+            }).toArray();
+
+            // console.log(classes);
+            res.send(classes);
+        });
         app.get('/class/:id', async (req, res) => {
             const id = req.params.id;
             const singleClass = await classCollection.findOne({ _id: new ObjectId(id) })
@@ -53,14 +64,43 @@ async function run() {
                 res.send({ message: "Email Not found" })
             }
 
-            // const isUserExist = await userCollection.findOne({ email: email })
-            // if (isUserExist) {
-            //     return res.send({ message: "user is alrady exist" })
-            // }
+            const isUserExist = await userCollection.findOne({ email: email })
+            if (isUserExist) {
+                return res.send({ message: "user is alrady exist" })
+            }
             const user = await userCollection.insertOne(data)
             // console.log(classes)
             res.send(user)
         })
+        app.post('/join-class', async (req, res) => {
+            const { joinCode, email } = req.body;
+            console.log({ joinCode, email })
+
+            if (!email) {
+                return res.status(400).send({ message: "Email is required" });
+            }
+
+            const query = { joinCode };
+            const selectedClass = await classCollection.findOne(query);
+
+            if (!selectedClass) {
+                return res.status(404).send({ message: "Class not found" });
+            }
+
+            if (selectedClass?.students?.includes(email)) {
+                return res.status(400).send({ message: "Already Joined" });
+            }
+
+            await classCollection.updateOne(
+                { joinCode },
+                { $push: { students: email } }
+            );
+
+            const updatedClass = await classCollection.findOne(query);
+            console.log(updatedClass)
+            res.send(updatedClass);
+        });
+
 
 
 
